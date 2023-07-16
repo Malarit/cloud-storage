83bd93dfb82e23aba33c0ac94ecc2bff31bfc498
Malarit <78352453+Malarit@users.noreply.github.com>
2023-07-16 06:34:25 +0300

    feat: add component to page

--- src/screens/pages/Cloud.tsx
+++ src/screens/pages/Cloud.tsx
@@ -1,57 +1,93 @@
 import CloudFile from "../../components/CloudFile";
+import menu from "../../assets/menu/menu white.svg";
+import pen from "../../assets/menu/pen white.svg";
+import download from "../../assets/menu/download white.svg"
+import deleteIcon from "../../assets/menu/delete white.svg"
 
 type cloudFileType = React.ComponentProps<typeof CloudFile>;
 
+const trackPopupList = [
+  {
+    icon: pen,
+    primary: false,
+    name: "",
+  },
+  {
+    icon: download,
+    primary: false,
+    name: "",
+  },
+  {
+    icon: menu,
+    primary: true,
+    name: "",
+  },
+  {
+    icon: deleteIcon,
+    primary: false,
+    name: "",
+  },
+];
+const files: Omit<cloudFileType, "trackPopup">[] = [
+  {
+    id: 0,
+    type: "folder",
+    name: "asd",
+    date: "10.11.2023",
+    size: "15MB",
+  },
+  {
+    id: 1,
+    type: "folder",
+    name: "asd",
+    date: "10.11.2023",
+    size: "15MB",
+  },
+  {
+    id: 2,
+    type: "folder",
+    name: "asd",
+    date: "10.11.2023",
+    size: "15MB",
+  },
+  {
+    id: 3,
+    type: "file",
+    name: "file.js",
+    date: "10.11.2023",
+    size: "15MB",
+  },
+  {
+    id: 4,
+    type: "file",
+    name: "file.js",
+    date: "10.11.2023",
+    size: "15MB",
+  },
+  {
+    id: 5,
+    type: "file",
+    name: "file.js",
+    date: "10.11.2023",
+    size: "15MB",
+  },
+];
 const Cloud: React.FC = () => {
-  const files: cloudFileType[] = [
-    {
-      id: 0,
-      type: "folder",
-      name: "asd",
-      date: "10.11.2023",
-      size: "15MB",
-    },
-    {
-      id: 1,
-      type: "folder",
-      name: "asd",
-      date: "10.11.2023",
-      size: "15MB",
-    },
-    {
-      id: 2,
-      type: "folder",
-      name: "asd",
-      date: "10.11.2023",
-      size: "15MB",
-    },
-    {
-      id: 3,
-      type: "file",
-      name: "file.js",
-      date: "10.11.2023",
-      size: "15MB",
-    },
-    {
-      id: 4,
-      type: "file",
-      name: "file.js",
-      date: "10.11.2023",
-      size: "15MB",
-    },
-    {
-      id: 5,
-      type: "file",
-      name: "file.js",
-      date: "10.11.2023",
-      size: "15MB",
-    },
-  ];
+  const onCLickPopup = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {};
 
   return (
     <>
       {files.map((file) => (
-        <CloudFile key={file.id} {...file} />
+        <CloudFile
+          trackPopup={{
+            list: trackPopupList,
+            onClick: onCLickPopup,
+          }}
+          key={file.id}
+          {...file}
+        />
       ))}
     </>
   );
