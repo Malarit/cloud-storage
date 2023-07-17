cc097a4867cc6598f53787794442dba72d3f59d4
Malarit <78352453+Malarit@users.noreply.github.com>
2023-07-18 01:19:52 +0300

    feat: add filter component

--- src/screens/pages/Cloud.tsx
+++ src/screens/pages/Cloud.tsx
@@ -1,10 +1,12 @@
 import CloudFile from "../../components/CloudFile";
 import menu from "../../assets/menu/menu white.svg";
 import pen from "../../assets/menu/pen white.svg";
-import download from "../../assets/menu/download white.svg"
-import deleteIcon from "../../assets/menu/delete white.svg"
+import download from "../../assets/menu/download white.svg";
+import deleteIcon from "../../assets/menu/delete white.svg";
+import Filter from "../../components/Filter";
 
 type cloudFileType = React.ComponentProps<typeof CloudFile>;
+type filterButtons = React.ComponentProps<typeof Filter>["buttons"];
 
 const trackPopupList = [
   {
@@ -72,6 +74,43 @@ const files: Omit<cloudFileType, "trackPopup">[] = [
     size: "15MB",
   },
 ];
+
+const filterListButtons: filterButtons = [
+  {
+    text: "Название",
+    popup: [
+      { text: "png", icon: "", cbValue: "" },
+      { text: "jpeg", icon: "", cbValue: "" },
+    ],
+    toggle: false,
+  },
+  {
+    text: "Файл",
+    popup: [
+      { text: "Папка", icon: "", cbValue: "" },
+      { text: "png", icon: "", cbValue: "" },
+      { text: "jpeg", icon: "", cbValue: "" },
+    ],
+    toggle: false,
+  },
+  {
+    text: "Дата",
+    popup: [
+      { text: "По возрастанию", icon: "", cbValue: "" },
+      { text: "По убыванию", icon: "", cbValue: "" },
+    ],
+    toggle: false,
+  },
+  {
+    text: "Размер файла",
+    popup: [
+      { text: "По возрастанию", icon: "", cbValue: "" },
+      { text: "По убыванию", icon: "", cbValue: "" },
+    ],
+    toggle: false,
+  },
+];
+
 const Cloud: React.FC = () => {
   const onCLickPopup = (
     e: React.MouseEvent<HTMLButtonElement, MouseEvent>
@@ -79,6 +118,7 @@ const Cloud: React.FC = () => {
 
   return (
     <>
+      <Filter buttons={filterListButtons} />
       {files.map((file) => (
         <CloudFile
           trackPopup={{
