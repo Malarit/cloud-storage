d7d1a22703b0711feddd8ca815e540df031814b0
Malarit <78352453+Malarit@users.noreply.github.com>
2023-07-22 23:45:08 +0300

    feat: add modal

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,27 +1,36 @@
 import { observer } from "mobx-react-lite";
-import Flex from "../components/Flex";
-import Grid from "../components/Grid";
-import Title from "../components/Title";
-import Wrapper from "../components/Wrapper";
-import MenuContainer from "../containers/home/MenuContainer";
-import ModalNewFolderContainer from "../containers/home/ModalNewFolderContainer";
+
 import Modal, { modals } from "../store/files";
 import Screens, { screens } from "../store/screens";
 import { theme } from "../Theme";
+
+import Wrapper from "../components/Wrapper";
+import Search from "../components/Search";
+import Title from "../components/Title";
+import Flex from "../components/Flex";
+import Grid from "../components/Grid";
+
 import UploadingFilesContainer from "../containers/home/UploadingFilesContainer";
 import DropFilesPageContainer from "../containers/home/DropFilesPageContainer";
+import ModalDeleteFile from "../containers/home/ModalDeleteFile";
 import UploadContainer from "../containers/home/UloadContainer";
-import Cloud from "../screens/pages/Cloud";
-import Search from "../components/Search";
+import MenuContainer from "../containers/home/MenuContainer";
+import CloudContainer from "../containers/home/cloud/CloudContainer";
+import CloudContainerTrash from "../containers/home/trash/CloudContainerTrash";
+import ModalRecoverFile from "../containers/home/ModalRecoverFile";
+import ModalNewFolderContainer from "../containers/home/ModalNewFolderContainer";
 
 const JSX_SCREENS: { [key in screens]: JSX.Element } = {
-  cloud: <Cloud />,
+  cloud: <CloudContainer />,
+  trash: <CloudContainerTrash />,
   profile: <Title color="#fff">3</Title>,
-  basket: <Title color="#fff">4</Title>,
 };
 
 const JSX_MODALS: { [key in modals]: JSX.Element } = {
+  delete: <ModalDeleteFile />,
+  recover: <ModalRecoverFile />,
   "new folder": <ModalNewFolderContainer />,
+  "update name": <></>,
   "upload file": <></>,
   "upload folder": <></>,
 };
@@ -37,7 +46,7 @@ const Home: React.FC = observer(() => {
           height="100%"
           templateColumns="max(250px, 15%) 1fr"
         >
-          <Flex direction="column">
+          <Flex maxHeight="100vh" direction="column">
             <Wrapper minHeight="3.5em" margin="0 1em" width="100%">
               <Title height="100%" fontSize="medium" color="#fff">
                 <Flex align="center">Logo</Flex>
@@ -48,7 +57,11 @@ const Home: React.FC = observer(() => {
               <UploadingFilesContainer />
             </Wrapper>
           </Flex>
-          <Wrapper padding="0 0 0 1em" borderRadius={theme.border.radius}>
+          <Wrapper
+            maxHeight="100vh"
+            padding="0 0 0 1em"
+            borderRadius={theme.border.radius}
+          >
             <Wrapper height="3.5em">
               <Search />
             </Wrapper>
