0013b44bb4414a477886301f2ecec2ad343da502
Malarit <78352453+Malarit@users.noreply.github.com>
2023-07-22 23:41:03 +0300

    refactor component

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,48 +1,83 @@
-import styled from "styled-components";
-import Button from "./Button";
+import React from "react";
+import ButtonWithIcon from "./ButtonWithIcon";
 import Flex from "./Flex";
 import Menu from "./Menu";
 import Popup from "./Popup";
-import ButtonWithIcon from "./ButtonWithIcon";
 
-type menu = React.ComponentProps<typeof Menu>["list"];
+type button = {
+  text: string;
+  icon: string;
+  key: string;
+  setActive: boolean;
+};
 
 type filter = {
-  buttons: {
-    text: string;
-    popup: menu;
-    toggle: boolean;
-  }[];
+  list: button[];
+  onClick?: (key: string) => void;
 };
 
 const Filter: React.FC<filter> = (props) => {
-  const { buttons } = props;
+  const { list, onClick } = props;
+
+  const refFlex = React.useRef<HTMLDivElement>(null);
+  const [popupActive, setPopupActive] = React.useState<boolean>(false);
+  const [active, setActive] = React.useState<button | undefined>(undefined);
+
+  const activeButton =
+    active || list.find(({ setActive }) => setActive) || list[0];
+
+  const popupList = list.map((button) => {
+    const { key, setActive, ...nextItems } = button;
+    return { cbValue: key, ...nextItems };
+  });
+
+  const onClickMenuButton = (key: string) => {
+    setActive(list.find((button) => button.key === key));
+    onClick?.(key);
+    setPopupActive(false)
+  };
+
+  React.useEffect(() => {
+    const onWindowClick = (e: MouseEvent) => {
+      if (!refFlex.current) return;
+      if (!e.composedPath().includes(refFlex.current)) {
+        setPopupActive(false);
+      }
+    };
+    window.addEventListener("click", onWindowClick);
+    return () => {
+      removeEventListener("click", onWindowClick);
+    };
+  }, []);
 
   return (
-    <Flex width="max-content">
-      {buttons.map(({ text, popup, toggle }, i) => (
-        <Popup
-          key={i}
-          width="auto"
-          toggleElement={
-            <ButtonWithIcon
-              marginIcon="0 1em 0 0"
-              icon={""}
-              text={text}
-              height="2.2em"
-              margin={i > 0 ? "0 0 0 1em" : ""}
-            />
-          }
-          popupElement={
-            <Menu
-              bgColor="#000"
-              padding="1.5em"
-              list={popup.map((item) => ({ ...item, bgColor: "#000" }))}
-            />
-          }
-          toggle={toggle}
-        />
-      ))}
+    <Flex ref={refFlex} width="12em" height="2.5em">
+      <Popup
+        width="auto"
+        height="max-content"
+        toggleElement={
+          <ButtonWithIcon
+            {...activeButton}
+            height="2.5em"
+            width="100%"
+            marginIcon="0 1em"
+            onClick={() => setPopupActive((curr) => !curr)}
+          />
+        }
+        popupElement={
+          <Menu
+            paddingWrapper="0.3em 0"
+            padding="1.3em"
+            bgColor="#000"
+            list={popupList.map((item) => ({
+              ...item,
+              bgColor: "transparent",
+            }))}
+            onClick={(key) => onClickMenuButton(key)}
+          />
+        }
+        toggle={popupActive}
+      />
     </Flex>
   );
 };
