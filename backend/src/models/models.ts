4d69217bc47679a717b9d9d14d6fa93052f8b486
Malarit <78352453+Malarit@users.noreply.github.com>
2023-07-28 19:36:53 +0300

    feat: add new models

--- backend/src/models/models.ts
+++ backend/src/models/models.ts
@@ -27,3 +27,74 @@ export const User: mt.user_model = sequelize.define("User", {
     allowNull: false,
   },
 });
+
+export const Cloud: mt.cloud_model = sequelize.define("Cloud", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  type: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  size: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+User.hasMany(Cloud, {
+  foreignKey: "userId",
+});
+
+export const Folder_Cloud: mt.folder_Cloud_model = sequelize.define(
+  "Folder_Cloud",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    folderId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    cloudId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  }
+);
+Cloud.hasMany(Folder_Cloud, {
+  foreignKey: "folderId",
+});
+Cloud.hasMany(Folder_Cloud, {
+  foreignKey: "cloudId",
+});
+
+export const Trash: mt.trash_model = sequelize.define("Trash", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  cloudId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+});
+User.hasMany(Trash, {
+  foreignKey: "userId",
+});
--- backend/src/models/types.ts
+++ backend/src/models/types.ts
@@ -1,11 +1,42 @@
 import { ModelDefined, Optional } from "sequelize";
 
-export type nodeAttributes<T, key extends keyof T> = Optional<T, key>;
+type nodeAttributes<T, key extends keyof T> = Optional<T, key>;
+type OptionalKeys<T extends object> = {
+  [P in keyof T]: {} extends Pick<T, P> ? P : never;
+}[keyof T];
+type Model<T extends {}> = ModelDefined<
+  Required<T>,
+  nodeAttributes<T, OptionalKeys<T>>
+>;
 
 interface user {
-  id: number;
+  id?: number;
   userName: string;
   email: string;
   password: string;
 }
-export type user_model = ModelDefined<user, nodeAttributes<user, "id">>;
+
+interface cloud {
+  id?: number;
+  userId: number;
+  type: string;
+  name: string;
+  size: string;
+}
+
+interface folder_Cloud {
+  id?: number;
+  folderId: number;
+  cloudId: number;
+}
+
+interface trash {
+  id?: number;
+  userId: number;
+  cloudId: number;
+}
+
+export type user_model = Model<user>;
+export type cloud_model = Model<cloud>;
+export type folder_Cloud_model = Model<folder_Cloud>;
+export type trash_model = Model<trash>;
