a9e6b9db63407fd49ce7ab5e4223a42810252281
Malarit <78352453+Malarit@users.noreply.github.com>
2023-07-28 19:40:25 +0300

    feat: add page navigation

--- frontend/src/route/App.tsx
+++ frontend/src/route/App.tsx
@@ -1,8 +1,11 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
 import Login from "../pages/login";
 import Home from "../pages/home";
+import React from "react";
+import account from "../store/account";
+import { observer } from "mobx-react-lite";
 
 const StyledApp = styled.div`
   max-width: 100vw;
@@ -11,7 +14,28 @@ const StyledApp = styled.div`
   overflow: hidden;
 `;
 
-function App() {
+const App = observer(() => {
+  const navigate = useNavigate();
+  const refFlag = React.useRef<boolean>(true);
+
+  React.useEffect(() => {
+    if (refFlag.current) {
+      account.requestUserId();
+    }
+    refFlag.current = false;
+
+    const timer = setInterval(() => {
+      account.requestUserId();
+    }, 1000 * 1800); // 30 min
+    return () => clearInterval(timer);
+  }, []);
+
+  // React.useEffect(() => {
+  //   if (account.userId === -1) return;
+  //   if (!account.userId) navigate("/login");
+  //   else navigate("/");
+  // }, [account.userId]);
+
   return (
     <StyledApp>
       <Routes>
@@ -20,6 +44,6 @@ function App() {
       </Routes>
     </StyledApp>
   );
-}
+});
 
 export default App;
