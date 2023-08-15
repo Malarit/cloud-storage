d0f7d837cb1695d94fa91a9b341ed358aef1b521
Malarit <78352453+Malarit@users.noreply.github.com>
2023-08-15 21:41:25 +0300

    refactor: minor changes

--- backend/src/middleware/upload.ts
+++ backend/src/middleware/upload.ts
@@ -1,13 +1,16 @@
 import multer from "multer";
 import { Request } from "express";
 import { config } from "../config/config.js";
-
+//
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, config.saveFileDestination);
   },
   filename: (req, file, cb) => {
-    const fileName = Date.now() + "-" + file.fieldname + "." + file.mimetype;
+    const originalname = Buffer.from(file.originalname, "latin1").toString(
+      "utf8"
+    );
+    const fileName = Date.now() + "#-#" + originalname;
     cb(null, fileName);
   },
 });
