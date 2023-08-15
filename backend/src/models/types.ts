4a45b535d4f9d787eb51db932c870ae1ce71e4df
Malarit <78352453+Malarit@users.noreply.github.com>
2023-08-15 21:42:47 +0300

    refactor: update model types

--- backend/src/models/types.ts
+++ backend/src/models/types.ts
@@ -4,20 +4,20 @@ type nodeAttributes<T, key extends keyof T> = Optional<T, key>;
 type OptionalKeys<T extends object> = {
   [P in keyof T]: {} extends Pick<T, P> ? P : never;
 }[keyof T];
-type Model<T extends {}> = ModelDefined<
-  Required<T>,
-  nodeAttributes<T, OptionalKeys<T>>
+type Model<T extends {}, keys extends keyof T> = ModelDefined<
+  T,
+  nodeAttributes<T, keys>
 >;
 
 interface user {
-  id?: number;
+  id: number;
   userName: string;
   email: string;
   password: string;
 }
 
-interface cloud {
-  id?: number;
+export interface cloud {
+  id: number;
   userId: number;
   type: string;
   name: string;
@@ -25,18 +25,18 @@ interface cloud {
 }
 
 interface folder_Cloud {
-  id?: number;
+  id: number;
   folderId: number;
   cloudId: number;
 }
 
 interface trash {
-  id?: number;
+  id: number;
   userId: number;
   cloudId: number;
 }
 
-export type user_model = Model<user>;
-export type cloud_model = Model<cloud>;
-export type folder_Cloud_model = Model<folder_Cloud>;
-export type trash_model = Model<trash>;
+export type user_model = Model<user, "id">;
+export type cloud_model = Model<cloud, "id" | "size">;
+export type folder_Cloud_model = Model<folder_Cloud, "id">;
+export type trash_model = Model<trash, "id">;
