a3a76f0c05065af1d1b6c7ecd31c2f8fa30c12fa
Malarit <78352453+Malarit@users.noreply.github.com>
2023-08-15 21:54:08 +0300

    feat: add queries

--- frontend/src/hooks/queries.ts
+++ frontend/src/hooks/queries.ts
@@ -1,20 +1,28 @@
-import { useMutation, UseMutationOptions, useQuery } from "react-query";
-import { post } from "../services/requests";
+import {
+  QueryKey,
+  useMutation,
+  UseMutationOptions,
+  useQuery,
+  UseQueryOptions,
+  UseQueryResult,
+} from "react-query";
+import { get, post } from "../services/requests";
 import { AxiosResponse } from "axios";
 
 type mutationOption = Omit<
   UseMutationOptions<any, any, any, any>,
   "mutationFn"
 >;
+type queryOption = Omit<UseQueryOptions<any, any, any, any>, "queryKey">;
 
 function mutation<T>(
   fn: (obj: T) => Promise<AxiosResponse<any, any>>,
   option?: mutationOption
 ) {
-  return useMutation({
-    mutationFn: (obj: T) => fn(obj),
-    ...option,
-  });
+  return useMutation({ mutationFn: (obj: T) => fn(obj), ...option });
+}
+function query<T>(name: QueryKey, fn: () => Promise<T>, option?: queryOption) {
+  return useQuery(name, () => fn(), option) as UseQueryResult<T, any>;
 }
 
 export const authorization = (option?: mutationOption) => {
@@ -27,4 +35,15 @@ export const registration = (option?: mutationOption) => {
 
 export const cloud_mutation = (option?: mutationOption) => {
   return mutation(post.cloud, option);
+};
+
+export const cloud_query = (
+  params?: { filter?: string; folderId?: number },
+  option?: queryOption
+) => {
+  return query(
+    ["cloud", params?.filter, params?.folderId],
+    () => get.cloud(params),
+    option
+  );
 };
