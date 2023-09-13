16a6cd167d01f802bb0513fff2a29a0789a42605
Malarit <78352453+Malarit@users.noreply.github.com>
2023-09-13 21:16:39 +0300

    feat: add new components

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -5,14 +5,13 @@ import Screens, { screens } from "../store/screens";
 import { theme } from "../Theme";
 
 import Wrapper from "../components/Wrapper";
-import Search from "../components/Search";
 import Title from "../components/Title";
 import Flex from "../components/Flex";
 import Grid from "../components/Grid";
 
 import UploadingFilesContainer from "../containers/home/UploadingFilesContainer";
 import DropFilesPageContainer from "../containers/home/DropFilesPageContainer";
-import ModalDeleteFile from "../containers/home/ModalDeleteFile";
+import ModalDeleteFile from "../containers/home/cloud/ModalDeleteFile";
 import UploadContainer from "../containers/home/UploadContainer";
 import MenuContainer from "../containers/home/MenuContainer";
 import CloudContainer from "../containers/home/cloud/CloudContainer";
@@ -20,57 +19,65 @@ import CloudContainerTrash from "../containers/home/trash/CloudContainerTrash";
 import ModalRecoverFile from "../containers/home/ModalRecoverFile";
 import ModalNewFolderContainer from "../containers/home/ModalNewFolderContainer";
 import ModalUpdateNameContainer from "../containers/home/ModalUpdateNameContainer";
+import SearchContainer from "../containers/home/SearchContainer";
+import DownloadContainer from "./../containers/home/DownloadContainer";
+import ModalDeleteFileTrash from "../containers/home/trash/ModalDeleteFileTrash";
+import Profile from "../containers/profile";
 
 const JSX_SCREENS: { [key in screens]: JSX.Element } = {
   cloud: <CloudContainer />,
   trash: <CloudContainerTrash />,
-  profile: <Title color="#fff">3</Title>,
+  profile: <Profile />,
 };
 
 const JSX_MODALS: { [key in modals]: JSX.Element } = {
   delete: <ModalDeleteFile />,
   recover: <ModalRecoverFile />,
+  download: <DownloadContainer />,
+  "delete trash": <ModalDeleteFileTrash />,
   "new folder": <ModalNewFolderContainer />,
-  "update name": <ModalUpdateNameContainer/>,
+  "update name": <ModalUpdateNameContainer />,
   "upload file": <></>,
   "upload folder": <></>,
 };
 
 const Home: React.FC = observer(() => {
   return (
-    <DropFilesPageContainer>
-      <UploadContainer />
-      <Wrapper minHeight="100vh">
-        {Modal.activeModal && JSX_MODALS[Modal.activeModal]}
-        <Grid
-          minHeight="100vh"
-          height="100%"
-          templateColumns="max(250px, 15%) 1fr"
-        >
-          <Flex maxHeight="100vh" direction="column">
-            <Wrapper minHeight="3.5em" margin="0 1em" width="100%">
-              <Title height="100%" fontSize="medium" color="#fff">
-                <Flex align="center">Logo</Flex>
-              </Title>
-            </Wrapper>
-            <MenuContainer />
-            <Wrapper height="100%" position="relative">
-              <UploadingFilesContainer />
-            </Wrapper>
-          </Flex>
-          <Wrapper
-            maxHeight="100vh"
-            padding="0 0 0 1em"
-            borderRadius={theme.border.radius}
+    <Wrapper padding="0 1em">
+      <DropFilesPageContainer>
+        <UploadContainer />
+        <Wrapper minHeight="100vh">
+          {Modal.activeModal && JSX_MODALS[Modal.activeModal]}
+          <Grid
+            minHeight="100vh"
+            height="100%"
+            templateColumns="max(250px, 15%) 1fr"
           >
-            <Wrapper height="3.5em">
-              <Search />
+            <Flex maxHeight="100vh" direction="column">
+              <Wrapper minHeight="3.5em" margin="0 1em" width="100%">
+                <Title height="100%" fontSize="medium" color="#fff">
+                  <Flex align="center">Logo</Flex>
+                </Title>
+              </Wrapper>
+              <MenuContainer />
+              <Wrapper height="100%" position="relative">
+                <UploadingFilesContainer />
+              </Wrapper>
+            </Flex>
+            <Wrapper
+              maxHeight="100vh"
+              padding="0 0 0 1em"
+              borderRadius={theme.border.radius}
+            >
+              <Wrapper height="3.5em">
+                <SearchContainer />
+              </Wrapper>
+              <Wrapper>{JSX_SCREENS[Screens.activeScreen]}</Wrapper>
             </Wrapper>
-            <Wrapper>{JSX_SCREENS[Screens.activeScreen]}</Wrapper>
-          </Wrapper>
-        </Grid>
-      </Wrapper>
-    </DropFilesPageContainer>
+          </Grid>
+        </Wrapper>
+      </DropFilesPageContainer>
+    </Wrapper>
   );
 });
 
--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,7 +1,9 @@
 import Flex from "../components/Flex";
 import Grid from "../components/Grid";
+import Icon from "../components/Icon";
 import Wrapper from "../components/Wrapper";
 import AuthContainer from "../containers/login/AuthContainer";
+import imgNegate from "../assets/menu/imgNegate.png";
 
 const Login: React.FC = () => {
   return (
@@ -10,7 +12,14 @@ const Login: React.FC = () => {
         height="100vh"
         templateColumns="1fr clamp(320px, 100%, min(750px, 60%))"
       >
-        <div></div>
+        <Flex height="100vh">
+          <Icon
+            objectFit="contain"
+            maxHeight="100%"
+            maxWidth="100%"
+            src={imgNegate}
+          />
+        </Flex>
         <Wrapper bgColor="#3e3e3e">
           <Wrapper margin="0 auto" height="100%" maxWidth="40%">
             <Flex align="center">
