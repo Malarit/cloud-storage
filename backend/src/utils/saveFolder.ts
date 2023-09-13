cbd24155c034ce1c423339399d1c49b5a79cecb8
Malarit <78352453+Malarit@users.noreply.github.com>
2023-09-13 20:34:39 +0300

    refactor: add await

--- backend/src/utils/saveFolder.ts
+++ backend/src/utils/saveFolder.ts
@@ -1,25 +1,24 @@
-import Folder from "../../classes/Folder/Folder.js";
-import { Cloud, Folder_Cloud } from "../../models/models.js";
-import { cloud } from "../../models/types.js";
+import Folder from "../classes/Folder/Folder.js";
+import { Cloud, Folder_Cloud } from "../models/models.js";
+import { cloud } from "../models/types.js";
 
 type resvalue = {
   fileId: number;
   size: string;
 };
-type save_folder = (folder: Folder<cloud>, userId: number) => void;
+type save_folder = (folder: Folder<cloud>, userId: number) => Promise<any>;
 type saveType = (
   folder: (resvalue | undefined)[],
   folderName: string,
   userId: number
 ) => any;
-
-const save: saveType = async (folder, folderName, userId) => {
+const save: saveType = async (folder, parentFolderName, userId) => {
   const folderData = folder.reduce<{ fileId: number[]; size: number }>(
     (prev, curr) => {
       if (!curr) return prev;
       const { fileId, size } = curr;
       prev.fileId.push(fileId);
-      prev.size += Number.parseFloat(size);
+      prev.size += Number(size);
       return prev;
     },
     { fileId: [], size: 0 }
@@ -28,17 +27,18 @@ const save: saveType = async (folder, folderName, userId) => {
   const folderDB = await Cloud.create({
     userId,
     type: "folder",
-    name: folderName,
-    size: Math.round(folderData.size) + "KB",
+    name: parentFolderName,
+    size: folderData.size,
   }).then((val) => val.get({ plain: true }));
-
-  folderData.fileId.map((id) => {
-    Folder_Cloud.build({
-      folderId: folderDB.id,
-      cloudId: id,
-    }).save();
-  });
-  return { fileId: folderDB.id, size: Math.round(folderData.size) + "KB" };
+  await Promise.all(
+    folderData.fileId.map(async (id) => {
+     await Folder_Cloud.build({
+        folderId: folderDB.id,
+        cloudId: id,
+      }).save();
+    })
+  );
+  return { fileId: folderDB.id, size: folderData.size };
 };
 
 function moveStruct(
@@ -59,7 +59,7 @@ function moveStruct(
 
 const saveFolder: save_folder = async (folder, userId) => {
   const files = await Promise.all(moveStruct(folder, userId));
-  save(files, folder.folderName, userId);
+  await save(files, folder.folderName, userId);
 };
 
 export default saveFolder;
