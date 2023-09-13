8b192d1037bb18442885c46308b916d17668389a
Malarit <78352453+Malarit@users.noreply.github.com>
2023-09-13 21:05:50 +0300

    feat: add new style

--- frontend/src/components/Icon.tsx
+++ frontend/src/components/Icon.tsx
@@ -2,16 +2,20 @@ import styled from "styled-components";
 
 type icon = {
   maxWidth?: string;
+  maxHeight?: string;
   width?: string;
   height?: string;
   margin?: string;
+  objectFit?: string;
 };
 
 const Icon = styled.img<icon>`
   width: ${({ width }) => width};
   max-width: ${({ width }) => width || "100%"};
+  max-height: ${({ maxHeight }) => maxHeight};
   height: ${({ height }) => height};
   margin: ${({ margin }) => margin};
+  object-fit: ${({ objectFit }) => objectFit};
 `;
 
 export default Icon;
