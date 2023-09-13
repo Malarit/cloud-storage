010cccaaa799e8b91a1e42321e4da016e38a655a
Malarit <78352453+Malarit@users.noreply.github.com>
2023-09-13 20:38:39 +0300

    feat: add new routes

--- backend/src/routes/cloud.ts
+++ backend/src/routes/cloud.ts
@@ -5,9 +5,13 @@ import * as controller from "../controllers/cloud/cloud.controller.js";
 const router = Router();
 
 router.post("/cloud", upload.array("files"), controller.set_file);
+router.post("/updateFolderCloud", controller.update_folder_cloud);
+router.post("/updateName", controller.update_name);
+router.post("/trash", controller.delete_file);
+router.post("/recoverFile", controller.recover_file);
 
 router.get("/cloud", controller.get_files);
 router.get("/folder", controller.get_folder);
-router.get("/updateName", controller.update_name);
+router.get("/download", controller.download);
 
 export default router;
--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -5,7 +5,10 @@ const router = Router();
 
 router.post("/authorization", controller.authorization);
 router.post("/registration", controller.registration);
+router.post("/updateUserData", controller.updateUserData);
 
 router.get("/itsMe", controller.itsMe);
+router.get("/userData", controller.userData);
+router.get("/exitLogin", controller.exitLogin);
 
 export default router;
