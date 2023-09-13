d404880dfbf0069c265b3fc9436f56fd31051104
Malarit <78352453+Malarit@users.noreply.github.com>
2023-09-13 21:16:50 +0300

    refactor

--- frontend/src/route/App.tsx
+++ frontend/src/route/App.tsx
@@ -1,16 +1,16 @@
 import { Route, Routes, useNavigate } from "react-router-dom";
+import { observer } from "mobx-react-lite";
 import styled from "styled-components";
 
 import Login from "../pages/login";
 import Home from "../pages/home";
 import React from "react";
 import account from "../store/account";
-import { observer } from "mobx-react-lite";
+import config from "../../config";
 
 const StyledApp = styled.div`
   max-width: 100vw;
   width: 100%;
-  padding: 0 1em;
   overflow: hidden;
 `;
 
@@ -26,7 +26,7 @@ const App = observer(() => {
 
     const timer = setInterval(() => {
       account.requestUserId();
-    }, 1000 * 1800); // 30 min
+    }, config.ACCESS_TOKEN_TIMER);
     return () => clearInterval(timer);
   }, []);
 
