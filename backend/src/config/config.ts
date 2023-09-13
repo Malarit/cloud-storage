4f572dc12f02f0cbc3c8f192fb1889a2f6834eba
Malarit <78352453+Malarit@users.noreply.github.com>
2023-09-13 20:33:26 +0300

    feat: add new header

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -11,6 +11,7 @@ const headers = {
   "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE",
   "Access-Control-Allow-Headers": "X-Requested-With,content-type",
   "Access-Control-Allow-Credentials": "true",
+  "Access-Control-Expose-Headers": "X-Total-Count",
 };
 
 export const cors = new Cors(headers).setHeader();
