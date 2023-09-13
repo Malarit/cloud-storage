300e2fc5cae81bcbd5e4aefe7aac8fbacdd4b9bd
Malarit <78352453+Malarit@users.noreply.github.com>
2023-09-13 21:17:16 +0300

    feat: add export queries

--- frontend/src/services/requests/index.ts
+++ frontend/src/services/requests/index.ts
@@ -4,15 +4,31 @@ import {
   itsMe,
   cloud,
   cloud_get,
+  update_folder_cloud,
+  delete_file,
+  update_name,
+  recover_file,
+  download,
+  userData,
+  exitLogin,
+  updateUserData,
 } from "./requests";
 
 export const post = {
   authorization,
   registration,
   cloud,
+  update_folder_cloud,
+  delete_file,
+  update_name,
+  recover_file,
+  updateUserData,
 };
 
 export const get = {
   itsMe,
   cloud: cloud_get,
+  download,
+  userData,
+  exitLogin,
 };
