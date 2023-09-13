d8d0f0eef962a28503fa4d55cdfac126724b478c
Malarit <78352453+Malarit@users.noreply.github.com>
2023-09-13 21:12:05 +0300

    feat: add queries

--- frontend/src/containers/home/ModalUpdateNameContainer.tsx
+++ frontend/src/containers/home/ModalUpdateNameContainer.tsx
@@ -1,14 +1,30 @@
 import { observer } from "mobx-react-lite";
 import ModalSmall from "../../components/ModalSmall";
 import files from "../../store/files";
+import { update_name } from "../../hooks/queries";
+import useQueryFiles from "../../hooks/useQueryFiles";
 
 const ModalUpdateNameContainer: React.FC = observer(() => {
-  const onCLickSave = (value: string) => {
+  const { refetch } = useQueryFiles.query();
+  const update_name_mutation = update_name({
+    onSuccess() {
+      refetch();
+    },
+  });
+  
+  const onClickRight = (value: string) => {
+    const file = files.activeFile;
+    if (!file) return;
+
+    update_name_mutation.mutate({ id: file.id, name: value });
+
     files.removeActiveModals();
+    files.removeActiveFile();
   };
 
-  const onClickCancel = () => {
+  const onClickLeft = () => {
     files.removeActiveModals();
+    files.removeActiveFile();
   };
 
   return (
@@ -17,8 +33,8 @@ const ModalUpdateNameContainer: React.FC = observer(() => {
       inputValue={files.activeFile?.name}
       buttonLeftText="Отмена"
       buttonRightText="Сохранить"
-      onClickSave={onCLickSave}
-      onClickCancel={onClickCancel}
+      onClickRight={onClickRight}
+      onClickLeft={onClickLeft}
     />
   );
 });
